03ebab22d6630a1116c1e67c838fe62809585226
Bonelli <fabiobonelli.15@gmail.com>
2025-05-16 17:30:22 +0200

    created go js overview

--- HAS/app.py
+++ HAS/app.py
@@ -1,3 +1,6 @@
+import time
+from threading import Thread
+
 from flask import Flask, render_template, blueprints
 from Routes.overview_blueprint import overview_blueprint
 from Routes.vacuum_system_blueprint import vacuum_system_blueprint
@@ -7,7 +10,7 @@ from Routes.time_blueprint import time_blueprint
 from flask_cors import CORS
 from Services.http_clients import EswClient
 import threading
-
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.register_blueprint(overview_blueprint, url_prefix='/')
@@ -16,6 +19,16 @@ app.register_blueprint(hv_system_blueprint, url_prefix='/highvoltagesystem')
 app.register_blueprint(systemstatus_blueprint, url_prefix='/systemstatus')
 app.register_blueprint(time_blueprint, url_prefix='/time')
 CORS(app)
+
+socketio = SocketIO(app)
+
+def sendMessage():
+    while True:
+        socketio.emit('newDataAvailable', {'data': False})
+        time.sleep(3)
+
+        socketio.emit('newDataAvailable', {'data': True})
+        time.sleep(3)
 
 eswEndpoints = [
     "http://192.168.1.3/temperature_sensor_1",
@@ -28,4 +41,5 @@ client = EswClient(eswEndpoints)
 if __name__ == "__main__":
     api_thread = threading.Thread(target=client.start_fetching, daemon=True)
     api_thread.start()
-    app.run(host = "0.0.0.0" ,debug=True, port=5000)
+    socketio.start_background_task(sendMessage)
+    socketio.run(app ,host = "0.0.0.0" ,debug=True, port=5000, allow_unsafe_werkzeug=True)
